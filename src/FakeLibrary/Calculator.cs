1a4c374013217160f5f55f90a8cb65befac5682a
Jon <jashley.teach@gmail.com>
2026-08-18 17:13:07 +1000

    Basic docfx setup initialised

---
+++ src/FakeLibrary/Calculator.cs
@@ -0,0 +1,16 @@
+﻿namespace Tools;
+
+/// <summary>
+/// Provides simple mathematical operations.
+/// </summary>
+public class Calculator
+{
+    /// <summary>
+    /// Adds two numbers.
+    /// </summary>
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
+
