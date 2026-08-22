acd794df6abecc37bbc48155f018dcc92beee665
Jon <jashley.teach@gmail.com>
2026-08-22 21:02:37 +1000

    Added interface ITextTransformer

---
+++ src/FakeLibrary/ITextTransformer.cs
@@ -0,0 +1,21 @@
+﻿namespace JLib.Tools;
+
+/// <summary>
+/// Provides operations for transforming text.
+/// </summary>
+public interface ITextTransformer
+{
+    /// <summary>
+    /// Converts text to uppercase.
+    /// </summary>
+    /// <param name="text">The text to transform.</param>
+    /// <returns>The supplied text converted to uppercase.</returns>
+    public string ToUpper(string text);
+
+    /// <summary>
+    /// Emphasises text by placing an asterisk between each character.
+    /// </summary>
+    /// <param name="text">The text to transform.</param>
+    /// <returns>The transformed text with asterisks between characters.</returns>
+    public string Emphasise(string text);
+}
